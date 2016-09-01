fad1b189952b764893f79a5a23b8e6b27116287f
cre8mind <honsoj@gmail.com>
2016-09-01 10:13:37 -0500

    Update HomeWork7.java

--- HomeWork7.java
+++ HomeWork7.java
@@ -1,3 +1,10 @@
+/**
+ * Create a two dimensional array of type char, a game board that is 4 squares x 4 squares.  
+ * Populate each element of the array with a value of 
+ * either 'r' (for red piece), 'w' (for white piece) , or 'b' (for blank square).
+ * Then write a nested loop (either a for loop or enhanced for loop) to output the value of each square in order.
+ **/
+
 public class HomeWork7 {
 	public static void main(String[] args) {
 		char [][] matrix = {
