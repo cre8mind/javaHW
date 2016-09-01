6c937888602f03fa46a748a0426f197c3ad77cbe
cre8mind <honsoj@gmail.com>
2016-09-01 10:09:26 -0500

    Update HomeWork7.java

--- HomeWork7.java
+++ HomeWork7.java
@@ -1,1 +1,17 @@
 
+public class HomeWork7 {
+	public static void main(String[] args) {
+		char [][] matrix = {
+				{'r','r','w','w'},
+				{'b','b','w','w'},
+				{'r','r','r','r'},
+				{'w','r','b','b'}};
+
+		for(char [] array:matrix) {
+			for(char c:array) {
+				System.out.println(c);
+			}
+		}
+	}
+}
+
